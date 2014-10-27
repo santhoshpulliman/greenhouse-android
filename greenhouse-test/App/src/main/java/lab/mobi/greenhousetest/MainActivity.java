8629d8036cc6d5da41f5c2676b7298d4dd256153
Lauris Kruusamae <lauris.kruusamae@lab.mobi>
2014-10-27 14:38:12 +0200

    Add failing lint check

--- greenhouse-test/App/src/main/java/lab/mobi/greenhousetest/MainActivity.java
+++ greenhouse-test/App/src/main/java/lab/mobi/greenhousetest/MainActivity.java
@@ -1,15 +1,17 @@
 package lab.mobi.greenhousetest;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+
+import android.graphics.Bitmap;
 
 public class MainActivity extends Activity {
 
@@ -17,7 +19,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Bitmap bmp = Bitmap.createBitmap(null);
+        int size = bmp.getAllocationByteCount();
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
