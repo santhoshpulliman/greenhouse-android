9565f53ee7f34d9c9e7abcb6d3462aad775526ca
lauris <lauris.kruusamae@lab.mobi>
2015-08-07 13:37:58 +0300

    Add adb tests

--- greenhouse-test/App/src/androidTest/java/lab/mobi/greenhousetest/test/SuccessTest.java
+++ greenhouse-test/App/src/androidTest/java/lab/mobi/greenhousetest/test/SuccessTest.java
@@ -1,44 +1,46 @@
 package lab.mobi.greenhousetest.test;
 
 import android.test.AndroidTestCase;
-import java.lang.Exception;
+
+import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class SuccessTest extends AndroidTestCase {
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Test
+    public void testAdb() throws IOException, InterruptedException {
+        throw new RuntimeException("Installed apps list\n " + execCommand("adb shell pm list packages -3"));
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    @Test
+    public void testNoAdb() throws IOException, InterruptedException {
+        throw new RuntimeException("Installed apps list\n " + execCommand("pm list packages -3"));
     }
 
-    public void testSuccess1() throws Exception {
-        assertEquals(1, 1);
-        assertNotSame(1, 2);
-        assertTrue(true);
-        assertFalse(false);
-        assertTrue(true || false);
-        assertFalse(true && false);
+    public String execCommand(String command) throws IOException {
+        Process ps = Runtime.getRuntime().exec(command);
+        StringBuilder builder = new StringBuilder();
+        BufferedInputStream in = new BufferedInputStream(ps.getInputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return builder.toString();
     }
 
-    public void testSuccess2() throws Exception {
-        assertEquals(1, 1);
-        assertNotSame(1, 2);
-        assertTrue(true);
-        assertFalse(false);
-        assertTrue(true || false);
-        assertFalse(true && false);
-    }
-
-    public void testSuccess3() throws Exception {
-        assertEquals(1, 1);
-        assertNotSame(1, 2);
-        assertTrue(true);
-        assertFalse(false);
-        assertTrue(true || false);
-        assertFalse(true && false);
-    }
 }
